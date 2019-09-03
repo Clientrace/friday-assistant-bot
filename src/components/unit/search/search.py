cee310e6bd7b0c9da5ebc7e98642f594c3d765d6
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2019-09-03 09:40:44 +0800

    add empty response handler

--- src/components/unit/search/search.py
+++ src/components/unit/search/search.py
@@ -13,18 +13,21 @@ def exe(userID, data, response, altResponse, choice, optionMatched, valid, maxRe
 
   qresult = search.exe(data['data']['text'], userID)
   response = []
+
+  if( not qresult ):
+    response = spiel.text(userID, 'FS-02')
+    response += router.route(userID, 'main')
+    return response, valid
+
   if( 'suggestion' in qresult ):
     if( qresult['suggestion'] ):
       response += spiel.mod_text(userID, "FS-06", ":title:", qresult['suggestion'])
-
-  if( len(qresult['results']) == 0 ):
-    response = spiel.text(userID, 'FS-02')
-    response += router.route(userID, 'main')
 
   if( len(qresult['results']) == 1 ):
     summary = search.get_summary(qresult['results'][0]['pageid'])
     response += spiel.free_text(summary['title'], 0)
     response += spiel.free_text(summary['extract'], 0)
+    response += router.route(userID, 'main')
 
   if( len(qresult['results']) > 1 ):
     result_item = qresult['results'].pop(0)
@@ -35,9 +38,8 @@ def exe(userID, data, response, altResponse, choice, optionMatched, valid, maxRe
       relatedArticles += '* ' + r['title'] + '\n'
 
     response += spiel.free_text(relatedArticles, 0)
+    response += router.route(userID, 'main')
 
   return response, valid
-
-
 
 
