8df6b75909676f37b98e427dee54428a369cd950
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2019-09-05 07:56:19 +0800

    add simple query responses builder

--- src/_functions/search.py
+++ src/_functions/search.py
@@ -2,6 +2,7 @@
 Chatbot Search Functionality
 """
 
+import re
 import json
 import _uxy_core
 from src.wrappers.wiki import WikiQuery
@@ -9,28 +10,85 @@ from _uxy_core.utility.api_wrappers.facebook import Facebook
 
 wq = WikiQuery()
 
-def transform_wikimarkup():
-  pass
+def parse_wikimarkup(content):
+  responses = []
+  print(content)
+  headerRegEx = r'\=\=.*?\=\='
+  topicRegEx = r'\[\[.*?\]\]'
+  for section in content.split('\n\n')[1:]:
+    articleTitles = re.findall(headerRegEx, section)
+    if( len(articleTitles) > 0 ):
+      if( 'see also' not in articleTitles[0].replace('=','').strip().lower() ):
+        msgRes = 'In ' + articleTitles[0].replace('=','').strip()
+        for title in articleTitles[1:]:
+          msgRes += ', ' + title.replace('=','').strip()
+        msgRes += '\n'
+        articleTopics = re.findall(topicRegEx, section)
+        if( len(articleTopics) > 0 ):
+          msgRes += articleTopics[0].replace('[[','').replace(']]','')
+          for topic in articleTopics[1:]:
+            topic = topic.replace('[[', '').replace(']]', '')
+            msgRes += ', ' + topic
+          msgRes += '.'
+        responses.append(msgRes)
+    else: 
+      articleTopics = re.findall(topicRegEx, section)
+      if( len(articleTopics) > 0 ):
+        msgRes = articleTopics[0].replace('[[','').replace(']]','')
+        for topic in articleTopics:
+          msgRes += ', ' + topic.replace('[[','').replace(']]','')
+        msgRes += '.'
+        responses.append(msgRes)
+  return responses
 
 def find_disambiguation_page(pageIDs, pageInfos):
+  print('PAGE INFOS')
+  print(pageInfos)
   for pid in pageIDs:
     if( 'categories' in pageInfos[pid] ):
       page = wq.query_by_pageid(pid, 1)['query']['pages'][0]
       return page
   return None
 
+def check_result_relevance(querycheck, titlecheck):
+  if( len(querycheck) > len(titlecheck) ):
+    if titlecheck in querycheck:
+      return True
+
+  else:
+    if querycheck in titlecheck:
+      return True
+
+  return False
+
+
+def build_query_response(query, pageIDs, pageInfos):
+  responses = []
+  for pageID in pageIDs:
+    if( 'categories' not in pageInfos[pageID] ):
+      if( 'description' in pageInfos[pageID] ):
+        description = pageInfos[pageID]['description']
+        responses.append({
+          'title' : pageInfos[pageID]['title'],
+          'description' : description
+        })
+
+  return responses
+
+
 def exe(query):
-  searchResults = []
-  pages = wq.search(query)
+  pages = wq.search(query, 5)
   pageIDs = []
   for res in pages['query']['search']:
     pageIDs.append(str(res['pageid']))
 
-  if( len(pageIDs) == 0 ):
-    return None
-
   pageInfos = wq.get_description(pageIDs)['query']['pages']
-  ambiguationPage = find_disambiguation_page(pageIDs, pageInfos)
+  responses = build_query_response(query, pageIDs, pageInfos)
+  return responses
+
+
   
+
+
 
 
--- src/components/unit/search/search.py
+++ src/components/unit/search/search.py
@@ -13,40 +13,17 @@ def exe(userID, data, response, altResponse, choice, optionMatched, valid, maxRe
     if( maxRetry ):
       return [], valid
 
-  qresult = search.exe(data['data']['text'], userID)
-  response = []
-
-  if( not qresult ):
-    response = spiel.text(userID, 'FS-02')
-    response += router.route(userID, 'main')
+  qresult = search.exe(data['data']['text'])
+  if( len(qresult) == 0 ):
+    response = spiel.get_display(userID, 'FS-02')
     return response, valid
 
-  if( 'suggestion' in qresult ):
-    if( qresult['suggestion'] ):
-      response += spiel.mod_text(userID, "FS-06", ":title:", qresult['suggestion'])
+  response = []
+  for r in qresult:
+    response += spiel.free_text('['+r['title']+'] '+r['description'], 0)
 
-  if( len(qresult['results']) == 1 ):
-    summary = search.get_summary(qresult['results'][0]['pageid'], 10)
-    response += spiel.free_text(summary['title'], 0)
-    response += spiel.free_text(summary['extract'], 0)
-    response += router.route(userID, 'main')
-
-  if( len(qresult['results']) > 1 ):
-    fb = Facebook(userID, _uxy_core.environment.get('FACEBOOK', 'FB_PAGE_TOKEN'))
-    count = 0
-    for r in qresult['results']:
-      count += 1
-      summary = search.get_summary(r['pageid'], 1)
-      botMsg = str(count) + ' ' + summary['extract']
-      fb.send_txt_msg(botMsg)
-
-    
-    convo_data.save_item(userID, 'query_cache', json.dumps(qresult))
-    response += router.route(userID,'search.view_more')
-
-
+  response += router.route(userID, 'main')
   return response, valid
-
 
 
 
