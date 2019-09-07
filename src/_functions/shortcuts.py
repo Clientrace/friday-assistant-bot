6a66d53dae29f903ec66d63b94b6810f8b18c7eb
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2019-09-07 22:39:47 +0800

    fix shortcut matching

--- src/_functions/shortcuts.py
+++ src/_functions/shortcuts.py
@@ -5,7 +5,7 @@ def read(query):
   words = query.split(' ')
   if( len(words) > 1):
     if( '#read' in words[0] ):
-      return 'article.read_article'
+      return 'article.read'
 
   return None
 
