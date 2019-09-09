100813ffff2ebe88f9f9e171d9c70669f54c8585
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2019-09-10 07:25:16 +0800

    fix username info

--- _uxy_core/_modules/e2e/msg_platforms/fb_msg_parser.py
+++ _uxy_core/_modules/e2e/msg_platforms/fb_msg_parser.py
@@ -11,7 +11,7 @@ from _uxy_core.utility.api_wrappers.facebook import Facebook
 
 def parse_string_codes(fb, string_msg):
   if( ':username:' in string_msg ):
-    userName = fb.get_user_info('first_name', 'last_name')
+    userName = fb.get_user_info('first_name,last_name')
     userName = userName['first_name']+' '+userName['last_name']
     string_msg = string_msg.replace(':username:', userName)
 
